c260f2a7a48a7fe6202f3dde841dc28c98cbefe8
egonzalez99 <112592570+egonzalez99@users.noreply.github.com>
2024-02-07 23:25:32 -0500

    Final Version
    The serial monitor detects the loop. It works on the Arduino Uno.

--- CODE/timingmozzi_eg.ino
+++ CODE/timingmozzi_eg.ino
@@ -1,30 +1,40 @@
 #include <MozziGuts.h>
+#include <tables/sin1024_int8.h>
+#include <EventDelay.h>
+
 
 int counter = 0; //counter variable
-int interval = 1000; //interval for audioticks
+int interval = 64; //interval for audioticks. a sixteenth note is 1/4 a single beat. 256/4 = 64.
 int resetLimit = 15;//reset once limit is reach
 
-void start() {
 
-  startMozzi();
+void setup() {
+
+
+ startMozzi();
+ Serial.begin(115200);
+
 
 }
 
+
 void loop() {
+ int currentTick = audioTicks();
 
-  int currentTick = audioTicks();
 
-  //check if the ticks are incrementing pass zero
-  if(currentTick & audioTicks == 0) {
-    counter++;
+ //check if the ticks are incrementing pass zero
+ if(currentTick && audioTicks == 0) {
+   counter++;
 
-    //conce the limit is reached then it will reset when its incrementing
-    if(currentTick > resetLimit) {
-        count = 0;
-      }
-  }
-  //print the counter value
-  Serial.println(counter);
 
-  eventDelay(10); 
+   //conce the limit is reached then it will reset when its incrementing
+   if(currentTick > resetLimit) {
+       counter = 0;
+     }
+ }
+   //print the counter value
+   Serial.println(counter);
+
+
+   EventDelay(10);
 }
