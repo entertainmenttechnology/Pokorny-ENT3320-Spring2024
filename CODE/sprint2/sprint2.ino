ec03afb37ba0ab2fbf33308032676ebee28de6bc
KikeyGuan <113068463+KikeyGuan@users.noreply.github.com>
2024-02-27 16:55:48 -0500

    added enevlope

--- CODE/sprint2/sprint2.ino
+++ CODE/sprint2/sprint2.ino
@@ -1,48 +1,45 @@
+#include<Arduino.h>
 #include<MozziGuts.h>
 #include<Oscil.h>
 #include<tables/sin2048_int8.h>
 #include<RollingAverage.h>
 #include<mozzi_midi.h>
+#include<ADSR.h>
 
-// 1arduino, 6 piezo, audio jac, wires
 
 //arrays
-int PiezoCount[6]={A1, A2, A3, A4, A5, A0};//set up inputs
+int PiezoCount[6]={A1, A2, A3, A4, A5, A0};
 int PiezoAve[6]={1, 2, 3, 4, 5, 6};
 int MidiNote[6]={1, 2, 3, 4, 5, 6};
 
 //tigger limit (change)
 int trigger = 1;
 
-//set up pin numbers on arduino
-/*
-int pin1=A1;
-int pin2=A2;
-int pin3=A3;
-int pin4=A4;
-int pin5=A5;
-int pin6=A6;
-*/
-
+//get average for each PiezoCount
 RollingAverage<int, 8> pAverage1;
-RollingAverage<int, 8> pAverage2;
+RollingAverage<int, 12> pAverage2;
 RollingAverage<int, 8> pAverage3;
-RollingAverage<int, 8> pAverage4;
+RollingAverage<int, 12> pAverage4;
 RollingAverage<int, 8> pAverage5;
-RollingAverage<int, 8> pAverage6;
-
+RollingAverage<int, 12> pAverage6;
 
 Oscil <SIN2048_NUM_CELLS, AUDIO_RATE> aSin1(SIN2048_DATA);
+ADSR <AUDIO_RATE, AUDIO_RATE> envelope;
+
+//creating enevelope values
+int attack = 1;
+int decay = 100;
+int sustain = 100;
+int release = 1000;
+int AttackLevel = 225;
+int DecayLevel = 225;
 
 void setup() {
   // put your setup code here, to run once:
   startMozzi();
   Serial.begin(115200);
-
-}
-
-void updateControl(){
-  //set up analog Read
+  NoteDelay.set(1000);
+  //set up analog Read for mozzi
   PiezoCount[1] = mozziAnalogRead(A1);
   PiezoCount[2] = mozziAnalogRead(A2);
   PiezoCount[3] = mozziAnalogRead(A3);
@@ -50,6 +47,9 @@ void updateControl(){
   PiezoCount[5] = mozziAnalogRead(A5);
   PiezoCount[6] = mozziAnalogRead(A0);
 
+}
+
+void updateControl(){
   //get average for all the piezo input
   //for (int i=1;i<=6;i+=1){
     //PiezoAve[i] = kAverage.next(PiezoCount[i]);
@@ -63,13 +63,21 @@ void updateControl(){
 
   //turn piezo into midi note
   for (int i=1; i<=6; i+=1){
-    //MidiNote[i]= mtof(PiezoAve[i]);
+    MidiNote[i]= mtof(PiezoAve[i]);
   }
-  MidiNote[1] = mtof(PiezoAve[1]);
 
   //set frequency to midi note
   for (int i =1; i<=6; i+=1){
     aSin1.setFreq(MidiNote[i]);
+  }
+
+  //create enevlope
+  envelope.setADLevels(AttackLevel, DecayLevel);
+  envelope.setTimes(attack, decay, sustain, release);
+
+  if (PiezoCount[i]>trigger){
+    envelope.noteOn();
+    i++;
   }
 
   //testing area
@@ -81,16 +89,16 @@ void updateControl(){
 
 int updateAudio(){
   //for each piezo, if limit pass, play sound
-  for (int i = 0; i<=6; i+=1){
-    if (PiezoCount[i]< trigger ){
-      Serial.println (MidiNote[1]);
-      return MonoOutput::from8Bit((aSin1.next()));
-      
-    }
-  }
+  //for (int i = 0; i<=6; i+=1){
+    //if (PiezoCount[i]< trigger ){
+      //Serial.println (MidiNote[1]);
+      //return MonoOutput::from8Bit((aSin1.next()));
+    //}
+  //}
+  envelope.update();
+  return MonoOutput::from16Bit((int)(envelope.next()* aSin1.next()));
 }
 void loop() {
   // put your main code here, to run repeatedly:
   audioHook();
-
 }
