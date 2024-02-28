09fd8abae8c5b3e90caf670520950ff062ccc466
egonzalez99 <112592570+egonzalez99@users.noreply.github.com>
2024-02-27 19:47:07 -0500

    more optimized code of trigger Boolean

--- CODE/triggerbooleans_eg.ino
+++ CODE/triggerbooleans_eg.ino
@@ -1,40 +1,29 @@
-const int numOfSensors = 2; // Number of piezo sensors we are using. Can be modify
-const int pins[numOfSensors] = {A0, A1}; // Input the pin values on your arduino. this can be modify
-const float limit = 600; // Limit value to trigger the boolean
+const int numOfSensors = 1; // Number of piezo sensors we are using. Can be modify
+int pins[numOfSensors] = {A0}; // Input the pin values on your arduino. this can be modify
+int limit = 600; // Limit value to trigger the boolean
+int sensorVal[numOfSensors] = {0};
+
 
 bool trigger[numOfSensors] = {false}; // Status of the sensors inside an array
 
+
 void setup() {
-  Serial.begin(115200);
+ Serial.begin(115200);
 }
 
 void loop() {
-  // Checking the num of sensors while checking the status and limit
-  for (int i = 0; i < numOfSensors; i++) {
-    int sensorVal = analogRead(pins[i]); // New value to read each pin
+ // Checking the num of sensors while checking the status and limit
+ for (int i = 0; i < numOfSensors; i++) {
+    sensorVal[i] = analogRead(pins[i]); // New value to read each pin
 
-    trigger[i] = sensorVal > limit;
+    trigger[i] = sensorVal > limit;//device is trigger once the sensor values are greater than the limit
 
-    // Statement to print pin and trigger status
+    //Statement to print pin and trigger status
     Serial.print("Sensor: ");
-    Serial.print(i);
-
+    Serial.print(numOfSensors);
+    
     // Print sensor value
     Serial.print(", Value: ");
-    Serial.print(sensorVal);
-
-    // Print trigger status
-    Serial.print(", Triggered: ");
-
-    if (trigger[i]) {
-      Serial.println("triggered read");
-    } 
-    else {
-      Serial.println("triggered not read");
-    }
+    Serial.print(sensorVal[i]);
   }
-
-  Serial.println();
-  delay(10);
-
 }
