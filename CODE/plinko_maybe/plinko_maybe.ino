53123f963e6c24c4339d00f98544fc417df5d30d
Keyon19 <keyon.gray@mail.citytech.cuny.edu>
2024-04-11 16:58:03 -0400

    Needs Testing

--- CODE/plinko_maybe/plinko_maybe.ino
+++ CODE/plinko_maybe/plinko_maybe.ino
@@ -1,14 +1,13 @@
 #include <Arduino.h>
 #include <MozziGuts.h>
 #include <Oscil.h>
-#include <EventDelay.h>
 #include <ADSR.h>
 #include <mozzi_midi.h>
 #include <tables/triangle_warm8192_int8.h>
-#include <tables/sin8192_int8.h>
+//#include <tables/sin8192_int8.h>
 
 Oscil <8192, AUDIO_RATE> aTri(TRIANGLE_WARM8192_DATA);
-Oscil <8192, AUDIO_RATE> aSin(SIN8192_DATA);
+//Oscil <8192, AUDIO_RATE> aSin(SIN8192_DATA);
 
 ADSR <AUDIO_RATE, AUDIO_RATE> envelope;
 
@@ -20,6 +19,8 @@ int attack = 25,
     decay_level = 255;
 
 int i = 36;
+
+bool isPlaying = false; 
 
 void setup() 
 {
@@ -41,8 +42,11 @@ void updateControl()
 
   if (!digitalRead(8))
   {
+    isPlaying = !isPlaying;
     envelope.noteOn();
     i++;
+  } else {
+    isPlaying = isPlaying;
   }
 
   
@@ -52,7 +56,7 @@ void updateControl()
   }
 
   aTri.setFreq(mtof(i)); //converts MIDI note # to frequency
-  Serial.println((int)(envelope.next() * aTri.next()));
+  //Serial.println((int)(envelope.next() * aTri.next()));
 
  // Serial.println((int)(envelope.next()));
 }
@@ -60,8 +64,8 @@ void updateControl()
 AudioOutput_t updateAudio()
 {
   envelope.update();
-  return MonoOutput::from16Bit((int)(envelope.next() * aTri.next()));
-  
+  //return MonoOutput::from16Bit((int)(envelope.next() * aTri.next()));
+  return MonoOutput::from16Bit(aTri.next());
 }
 
 void loop()
